99b7ced699ffc1cdf2e29259051f801cedfd94f4
Shourabh <sourav.singh28@gmail.com>
2019-04-18 14:50:04 +0530

    Modified Python Code to find Palindrome

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,17 +1,20 @@
 ## Read a file to find the longest palindrome in it.
 ## 2 different functions..Use any
 
+import string
+
 file = '/tmp/file.txt'
 
 def palindrome_using_var(file):
     long_pal = ''
     content = open(file, 'r').read()
-    split_content = content.split()
-    
+    no_punc_content = content.translate(None, string.punctuation)
+    split_content = no_punc_content.split()
+
     for word in split_content:
         if word == word[::-1]:
   	    if len(long_pal) < len(word) :
-	       long_pal = word  
+	       long_pal = word
     print long_pal
 
 
@@ -19,15 +22,19 @@ def palindrome_using_list(file):
     list_pal = []
     long_pal = ''
     content = open(file, 'r').read()
-    split_content = content.split()
+    no_punc_content = content.translate(None, string.punctuation)
+    split_content = no_punc_content.split()
 
     for word in split_content:
         if word == word[::-1]:
-            list_pal.append(word)    
+            list_pal.append(word)
 
     for i in list_pal:
         if len(i) > len(long_pal):
 	    long_pal = i
-        
+
     print list_pal
     print long_pal
+
+palindrome_using_list(file)
+
