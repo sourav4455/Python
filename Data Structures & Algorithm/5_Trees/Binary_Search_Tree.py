71b1ee0085927431afb9b955c3dfbb40df57a8ad
Shourabh <sourav.singh28@gmail.com>
2020-02-23 01:59:25 +0530

    Added new section of Data Structure and Algorithm

--- Data Structures & Algorithm/5_Trees/Binary_Search_Tree.py
+++
@@ -1,5 +0,0 @@
-
-## A BST relies on the property that keys that are less then the parent are found in the left
-## subtree, and keys that are greater that the parent are found in the right subtree.
-
-Class BinarySearchTree:
